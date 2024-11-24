685aab996d4928f056c9b575a2414e65690e3b7e
Luis Diaz <luisdh.8@outlook.com>
2024-11-23 20:38:26 -0600

    Login with navigation

--- cias/src/App.jsx
+++ cias/src/App.jsx
@@ -1,14 +1,24 @@
-// src/App.jsx
-import React from 'react';
-import './styles/App.css';
-import Home from './userHome.jsx';
+import { useState } from 'react';
 import Login from './Login.jsx';
+import UserHome from './userHome.jsx';
+import './styles/App.css';
 
 function App() {
+  const [step, setStep] = useState(0);
+
+  const handleLogin = (success) => {
+    if (success) {
+      setStep(1); // Cambiar a la página de UserHome
+    } else {
+      alert('Invalid Credentials');
+    }
+  };
+
   return (
-    <div>
-      <Login />
-    </div>
+    <>
+      {step === 0 && <Login onLogin={handleLogin} />}
+      {step === 1 && <UserHome />}
+    </>
   );
 }
 
--- cias/src/Login.jsx
+++ cias/src/Login.jsx
@@ -1,33 +1,50 @@
 import React, { useState } from 'react';
 import './styles/Login.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; //Install bootstrap using npm install bootstrap
-import axios from 'axios'; //Install axios using npm install axios
+import 'bootstrap/dist/css/bootstrap.min.css'; // Install bootstrap using npm install bootstrap
+import axios from 'axios';
 
-function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    function handleSubmit(event){
-        event.preventDefault();
-        axios.post('http://localhost:8081/login', {email, password})
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-    }
+function Login({ onLogin }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.post('http://localhost:8081/login', { email, password })
+      .then(res => {
+        if (res.data === "Login Successful") {
+          onLogin(true); // Llamar a la función `onLogin` con `true` si el login fue exitoso
+        } else {
+          onLogin(false); // Llamar a la función `onLogin` con `false` si las credenciales son incorrectas
+        }
+      })
+      .catch(err => console.log(err));
+  }
 
   return (
     <div className="d-flex vh-100 justify-content-center align-items-center bg-primary">
-        <div className='p-3 bg-white w-25'>
-            <form onSubmit={handleSubmit}>
-                <div className='mb-3'>
-                    <label htmlFor='name'>Email</label>
-                    <input type = 'email' placeholder='Enter Email' className='form-control' onChange={e => setEmail(e.target.value)}/>
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor='name'>Password</label>
-                    <input type = 'password' placeholder='Enter Password' className='form-control' onChange={e => setPassword(e.target.value)}/>
-                </div>
-                <button className='btn btn-success'>Login</button>
-            </form>
-        </div>
+      <div className='p-3 bg-white w-25'>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-3'>
+            <label htmlFor='name'>Email</label>
+            <input
+              type='email'
+              placeholder='Enter Email'
+              className='form-control'
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='name'>Password</label>
+            <input
+              type='password'
+              placeholder='Enter Password'
+              className='form-control'
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <button className='btn btn-success'>Login</button>
+        </form>
+      </div>
     </div>
   );
 }
